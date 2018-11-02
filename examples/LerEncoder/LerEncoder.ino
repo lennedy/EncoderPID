4ffab0af47aa65e201692c808746e0ce820fdec5
lennedy <lennedy@gmail.com>
2018-11-01 21:17:41 -0300

    Criado Exemplo
    Criado exemplo para ler os pulsos de enconder que foram contabilizados

--- examples/LerEncoder/LerEncoder.ino
+++ examples/LerEncoder/LerEncoder.ino
@@ -1,9 +1,27 @@
 #include <Encoder.h>
 
+Encoder e(12, 11);
+
 void setup(){
-  
+  Serial.begin(9600);
+  e.config();
 }
 
 void loop(){
-  
+  e.lerPulso();
+	if(esperou(1000)){
+  	Serial.println(e.getNumPulsos());
+	}	
+}
+
+bool esperou(int milisegundos){
+	static int tempoAtual=0;
+	static int tempoAnterior=0;
+	
+	tempoAtual = millis();
+	if(tempoAtual-tempoAnterior > milisegundos){
+		tempoAnterior = tempoAtual;
+		return true;
+	}
+	return false;
 }
