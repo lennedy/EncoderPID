128f719f2daec971f8e0ab43cb0e7db4b406b844
lennedy <lennedy@gmail.com>
2018-11-01 22:53:13 -0300

    Criada classe motor
    Criada classe motor para conter todos os metodos que devem acionar o motor que será controlado.

---
+++ Motor.cpp
@@ -0,0 +1,25 @@
+#include "Motor.h"
+
+Motor::Motor(int pwmPin, int direcaoPin):PWM_PIN(pwmPin), DIRECAO_PIN(direcaoPin){
+	
+}
+
+void Motor::acionar(float percentualTensao){
+
+	if(percentualTensao < -100) 
+    percentualTensao = -100;	//se o valor passado em valor_por_cento for menor que -100 obriga-se o mmotor a ficar em -100
+  if(percentualTensao >  100) 
+    percentualTensao =  100;	//se o valor passado em valor_por_cento for maior que 100 obriga-se o mmotor a ficar em 100
+
+
+	if(percentualTensao < 0){
+		digitalWrite(DIRECAO_PIN, false);
+	}else{
+		digitalWrite(DIRECAO_PIN, true);
+		percentualTensao = 100 - percentualTensao;
+	}		
+
+  analogWrite(PWM_PIN, 255*(abs(percentualTensao))/100.0);
+	
+
+}
---
+++ Motor.h
@@ -0,0 +1,16 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <Arduino.h>
+#include <math.h>
+
+class Motor{
+private:
+	const int PWM_PIN, DIRECAO_PIN;
+public:
+	Motor(int pwmPin, int direcaoPin);
+	void acionar(float tensao);
+};
+
+#endif
+
