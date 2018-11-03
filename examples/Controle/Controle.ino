dd8c93499a55213353891a847eb8fbb05406fac2
lennedy <lennedy@gmail.com>
2018-11-03 20:50:05 -0300

    Modificado o exemplo
    Modificado o exemplo para permitir modificar periódicamente a posição do motor.

--- examples/Controle/Controle.ino
+++ examples/Controle/Controle.ino
@@ -1,13 +1,29 @@
 #include <ControleMotor.h>
 
 ControleMotor contrPos;
+Util tempo;
+bool primeira;
 
 void setup(){
   Serial.begin(9600);
   contrPos.config();
+  contrPos.setConstantes(1, 0.5, 0.1);
 }
 
 void loop(){
   
 	contrPos.loop();
+
+	if(tempo.esperou(1000)){
+		Serial.print("Angulo: ");
+		Serial.println(contrPos.getAngulo());
+		if(primeira){
+			contrPos.setSetpoint(90);
+			primeira=false;
+		}
+		else{
+			contrPos.setSetpoint(0);
+			primeira=true;
+		}
+	}
 }
