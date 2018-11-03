ef9b75bd35f0c19a564315216e6f94731ced4969
lennedy <lennedy@gmail.com>
2018-11-03 00:09:24 -0300

    Modificação na classe Util
    Modificação na classe Util para permitir vários objetos da mesma

--- Util.h
+++ Util.h
@@ -1,9 +1,14 @@
 class Util{
+private:
+		int tempoAtual;
+		int tempoAnterior;
 public:
+	Util(){
+		tempoAtual=0;
+		tempoAnterior=0;	
+	}
+
 	bool esperou(int milisegundos){
-		static int tempoAtual=0;
-		static int tempoAnterior=0;
-	
 		tempoAtual = millis();
 		if(tempoAtual-tempoAnterior > milisegundos){
 			tempoAnterior = tempoAtual;
